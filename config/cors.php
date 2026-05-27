6b800d79ffb28cde0f8048df266a3ad3feafd687
Sávio Gomes da Silva <saviogomesdasilvadev@gmail.com>
2026-05-27 11:01:34 -0300

    ajuste de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+   'allowed_origins' => ['https://sky-radar-web-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
