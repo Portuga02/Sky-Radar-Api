bb4aa1b028573c98e1458bea045d13bdad679666
Sávio Gomes da Silva <saviogomesdasilvadev@gmail.com>
2026-05-27 11:59:56 -0300

    Configurando rotas para servidor e local

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -8,19 +8,25 @@ class WeatherController extends Controller
 {
     public function getWeather($lat, $lon)
     {
-        $response = Http::get("https://api.hgbrasil.com/weather", [
+        $request = Http::withOptions([]);
+        if (app()->environment('local')) {
+            $request->withoutVerifying();
+        }
+        $response = $request->get("https://api.hgbrasil.com/weather", [
             'key'    => env('HG_BRASIL_KEY'),
             'lat'    => $lat,
             'lon'    => $lon,
             'format' => 'json'
         ]);
+
         return response()->json($response->json(), $response->status());
     }
+
     public function searchLocation($query)
     {
-
         $response = Http::withHeaders([
-            'User-Agent' => 'SkyRadarApp/1.0 (seuemail@provedor.com)'
+
+            'User-Agent' => 'SkyRadarApp/1.0 (saviogomesdasilvadev@gmail.com)'
         ])->get("https://nominatim.openstreetmap.org/search", [
             'format'         => 'json',
             'addressdetails' => 1,
